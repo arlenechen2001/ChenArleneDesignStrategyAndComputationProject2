a07a4cc69befb50de26f3e5557bdff46f699ca8a
arlenechen2001 <80081639+arlenechen2001@users.noreply.github.com>
2021-11-20 20:36:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// var AAbutton = document.getElementById("AAbutton")
+// var smallA = document.getElementById("smallA")
+// var bigA = document.getElementById("bigA")
+
+// function to show the accessibilityBar when AAbutton is pressed and hide when pressed again
+// if AAbutton is pressed show accessibilityBar
+// if AAbutton is pressed when accessibilityBar is shown hide accessibilityBar
+
+// function to increase and decrease text size
+// if bigA is pressed text size equals three px larger
+// if smallA is pressed text size equals three px small
+
+
+
+
+
+
